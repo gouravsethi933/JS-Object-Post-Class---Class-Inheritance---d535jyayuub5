466197387dcfccff93db21a4ada8b67800e0adfb
Gourav Sethi <61932647+gouravsethi933@users.noreply.github.com>
2022-01-20 18:11:12 +0530

    string

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,14 @@
 // import using require
+const Shape = require("./shape");
 
+class Circle extends Shape {
+  constructor(color) {
+    super();
+  }
+  calculateArea() {}
+}
+
+module.exports = Circle;
 // declare class
-
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,5 +1,11 @@
 // declare class
 class Shape {
+  constructor(color) {
+    this.color = "black";
+  }
+  calculateArea() {}
   drawShape() {}
 }
+
+module.exports = Shape;
 // export class using module.exports
